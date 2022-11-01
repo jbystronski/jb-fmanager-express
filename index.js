50b0a23d5e3fa9cbd659cb397fcd1f3f9c82b7e8
Jakub Bystroński <jbstr85@gmail.com>
2022-11-01 19:26:17 +0100

    initial

---
+++ index.js
@@ -0,0 +1,61 @@
+const fm = require("@jb_fmanager/node-utils");
+
+const fmRoutes = (app, { prefix = "/", errorHandler, maxUploadSize }) => {
+  const wrapAsync = async (fn, res, next, ...args) => {
+    try {
+      const result = await fn(...args);
+      res.status(200).json(result || {});
+    } catch (error) {
+      errorHandler ? errorHandler(error) : next(error);
+    }
+  };
+
+  app.get(
+    prefix + "/map",
+    async ({ query }, res, next) =>
+      await wrapAsync(fm.map, res, next, query.path)
+  );
+
+  app.get(
+    prefix + "/create_folder",
+    async ({ query }, res, next) =>
+      await wrapAsync(fm.create_folder, res, next, query.path, query.name)
+  );
+
+  app.get(
+    prefix + "/rename",
+    async ({ query }, res, next) =>
+      await wrapAsync(fm.rename, res, next, query.oldPath, query.newPath)
+  );
+
+  app.post(
+    prefix + "/remove",
+    async ({ body }, res, next) => await wrapAsync(fm.remove, res, next, body)
+  );
+
+  app.post(
+    prefix + "/copy",
+    async ({ query, body }, res, next) =>
+      await wrapAsync(fm.copy, res, next, query.target, body)
+  );
+
+  app.post(
+    prefix + "/move",
+    async ({ query, body }, res, next) =>
+      await wrapAsync(fm.move, res, next, query.target, body)
+  );
+
+  app.post(prefix + "/upload", async (req, res, next) => {
+    const uploadLimit = maxUploadSize || req.query.max_size;
+
+    try {
+      const pd = await fm.upload(req, res, req.query.destination, uploadLimit);
+
+      res.status(200).json(pd);
+    } catch (error) {
+      errorHandler ? errorHandler(error) : next(error);
+    }
+  });
+};
+
+module.exports = fmRoutes;
